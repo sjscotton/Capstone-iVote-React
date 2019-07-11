6fa9810e4baa86b3d95ee27f9cbab0a9e7d6fc54
sjscotton <sj.scotton@gmail.com>
2019-07-11 14:38:17 -0700

    added Material UI text fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 
+import TextField from '@material-ui/core/TextField';
+import './LoginForm.css'
+
+
+
 class LoginForm extends Component {
   constructor(props) {
     super(props);
@@ -36,56 +41,68 @@ class LoginForm extends Component {
 
   render() {
     return (
-      <form
-        onSubmit={this.onSubmit}>
-        <h3>First Name:</h3>
-        <input
-          name="firstName"
-          type="text"
-          value={this.state.firstName}
-          onChange={this.onInputChange}
-        >
-        </input>
-        <h3>Last Name:</h3>
-        <input
-          name="lastName"
-          type="text"
-          value={this.state.lastName}
-          onChange={this.onInputChange}
-        >
-        </input>
-        <h3>Birthdate</h3>
-        <h4>Month:</h4>
-        <input
-          name="month"
-          type="text"
-          value={this.state.month}
-          onChange={this.onInputChange}
-          placeholder="02"
-        ></input>
-        <h4>Day:</h4>
-        <input
-          name="day"
-          type="text"
-          value={this.state.day}
-          onChange={this.onInputChange}
-          placeholder="20"
-        ></input>
-        <h4>Year:</h4>
-        <input
-          name="year"
-          type="text"
-          value={this.state.year}
-          onChange={this.onInputChange}
-          placeholder="1984"
-        ></input>
+      <section>
+        <form
+          onSubmit={this.onSubmit}>
+          <h3>Name:</h3>
+          <TextField
+            className='mdc-text-field--outlined'
+            label='First Name'
+            variant="outlined">
+            <input
+              name="firstName"
+              type="text"
+              value={this.state.firstName}
+              onChange={this.onInputChange}
+            />
+          </TextField>
+          <h3>Last Name:</h3>
+          <TextField
+            className='mdc-text-field--outlined'
+            label='First Name'
+            variant="outlined">
+            <input
+              name="lastName"
+              type="text"
+              value={this.state.lastName}
+              onChange={this.onInputChange}
+            />
+          </TextField>
 
-        <button
-          type="submit"
-          name="submit"
-          value="search"
-        >Find me!</button>
-      </form >
+          <h3>Birthdate</h3>
+          <h4>Month:</h4>
+
+          <input
+            name="month"
+            type="text"
+            value={this.state.month}
+            onChange={this.onInputChange}
+            placeholder="02"
+          ></input>
+          <h4>Day:</h4>
+          <input
+            name="day"
+            type="text"
+            value={this.state.day}
+            onChange={this.onInputChange}
+            placeholder="20"
+          ></input>
+          <h4>Year:</h4>
+          <input
+            name="year"
+            type="text"
+            value={this.state.year}
+            onChange={this.onInputChange}
+            placeholder="1984"
+          ></input>
+
+          <button
+            type="submit"
+            name="submit"
+            value="search"
+          >Find me!</button>
+        </form >
+      </section >
     )
   }
 }
