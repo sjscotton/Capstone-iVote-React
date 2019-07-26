97fda8677acb813b10e8ab32068807b0cc8d2833
sjscotton <sj.scotton@gmail.com>
2019-07-26 14:56:17 -0700

    added social media icons, styled my reps page

--- src/components/Reps.js
+++ src/components/Reps.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee, FaFacebookSquare } from '@fortawesome/free-solid-svg-icons'
+import { FaFacebookSquare, FaTwitterSquare, FaYoutubeSquare } from "react-icons/fa";
+
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
@@ -12,16 +16,27 @@ import './Reps.css'
 
 const nonElectedPositions = ["Elections Director", "Assessor", "WA Supreme Court Justice", "State Auditor", "Commissioner of Public Lands", "Secretary of State", "State Treasurer", "Insurance Commissioner", "State Superintendent of Public Instruction"]
 
+
 class Reps extends Component {
 
   componentDidMount() {
     this.props.setCurrPageCallback('reps')
   }
 
+  getSocialMediaIcon(type) {
+    console.log(type)
+    if (type === 'Facebook') {
+      return (<FaFacebookSquare />)
+    } else if (type === 'Twitter') {
+      return (<FaTwitterSquare />)
+    } else {
+      return (<FaYoutubeSquare />)
+    }
+  }
   generateContactInfo(channels) {
     return channels.map((channel, i) => {
       return (
-        <li key={i}>{`${channel.type}: ${channel.id}`}</li>
+        <li key={i}>{this.getSocialMediaIcon(channel.type)}{`${channel.id}`}</li>
       )
     })
   }
@@ -30,7 +45,7 @@ class Reps extends Component {
     const officials = (this.props.formattedRepData) ? this.props.formattedRepData[region] : [];
 
     return officials.map((rep, i) => {
-      const email = (rep.emails) ? `Email: ${rep.emails[0]}` : '';
+      const email = (rep.emails) ? `${rep.emails[0]}` : '';
       const socialMedia = (rep.channels) ? this.generateContactInfo(rep.channels) : [];
       if (nonElectedPositions.includes(rep.title)) {
         return (<div key={i}></div>)
@@ -53,7 +68,7 @@ class Reps extends Component {
                 <div className="rep-flex">
                   <ul className="contact-info phone">
                     <li className="bold">Contact info:</li>
-                    <li>Phone: {rep.phones[0]}</li>
+                    <li>{rep.phones[0]}</li>
                     <li>{email}</li>
                   </ul>
                   <ul className="contact-info ">
@@ -83,17 +98,17 @@ class Reps extends Component {
       <div>
         <h1>My Representatives</h1>
         {/* <p>{this.props.address}</p> */}
-
-        <div className="panel-container">
-          <h3>City</h3>
-          <div>{this.generateReps('place')}</div>
-          <h3>County</h3>
-          <div>{this.generateReps('county')}</div>
-          <h3>Washington State</h3>
-          <div>{this.generateReps('state')}</div>
-          <h3>United States</h3>
-          <div>{this.generateReps('country')}</div>
-
+        <div className='flex-container'>
+          <div className="panel-container">
+            <h3>City</h3>
+            <div>{this.generateReps('place')}</div>
+            <h3>County</h3>
+            <div>{this.generateReps('county')}</div>
+            <h3>Washington State</h3>
+            <div>{this.generateReps('state')}</div>
+            <h3>United States</h3>
+            <div>{this.generateReps('country')}</div>
+          </div>
 
 
         </div>
